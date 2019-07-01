854fe71701a53ba79eb7b50287d3b947c830b192
Yixin Guo <blairguo1990@gmail.com>
2019-06-30 19:42:22 -0700

    Event Detail

--- src/index.js
+++ src/index.js
@@ -4,13 +4,16 @@ import './index.css';
 import HomeContainer from './Home/HomeContainer';
 import LoginContainer from './Login/LoginContainer';
 import SignupContainer from './Signup/SignupContainer';
+import PetDetailsContainer from './Signup/PetDetails/PetDetailsContainer.js';
 import * as serviceWorker from './serviceWorker';
 
 //ReactDOM.render(<HomeContainer />, document.getElementById('root'));
 
-ReactDOM.render(<LoginContainer />, document.getElementById('root'));
+//ReactDOM.render(<LoginContainer />, document.getElementById('root'));
 
 //ReactDOM.render(<SignupContainer />, document.getElementById('root'));
+
+ReactDOM.render(<PetDetailsContainer />, document.getElementById('root'));
 
 //ReactDOM.render(<MyInfo />, document.getElementById('root'));
 // If you want your app to work offline and load faster, you can change
