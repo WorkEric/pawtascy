5a09e6f9ceb1ecd3407b6f4f5301588234a31b2d
Yushu Pu <estelle19910112@163.com>
2019-07-02 00:24:36 -0700

    update

--- src/index.js
+++ src/index.js
@@ -6,6 +6,8 @@ import LoginContainer from './Login/LoginContainer';
 import SignupContainer from './Signup/SignupContainer';
 import PetDetailsContainer from './Signup/PetDetails/PetDetailsContainer.js';
 import ConfirmationContainer from './Signup/Confirmation/ConfirmationContainer.js';
+import HumanContainer from './Signup/HumanProfile/HumanContainer.js';
+
 import * as serviceWorker from './serviceWorker';
 
 //ReactDOM.render(<HomeContainer />, document.getElementById('root'));
@@ -16,8 +18,10 @@ import * as serviceWorker from './serviceWorker';
 
 //ReactDOM.render(<PetDetailsContainer />, document.getElementById('root'));
 
-ReactDOM.render(<ConfirmationContainer />, document.getElementById('root'));
+//ReactDOM.render(<ConfirmationContainer />, document.getElementById('root'));
 
+
+ReactDOM.render(<HumanContainer />, document.getElementById('root'));
 //ReactDOM.render(<MyInfo />, document.getElementById('root'));
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
