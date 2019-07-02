018b90886f136145359f8c1b257c837bdf383dc4
Yushu Pu <estelle19910112@163.com>
2019-07-02 00:23:38 -0700

    update

--- src/Signup/HumanProfile/HumanContainer.js
+++ src/Signup/HumanProfile/HumanContainer.js
@@ -1,48 +1,41 @@
 import React, { Component } from 'react';
-import SignComponent from './SignupComponent';
-import logo from '../images/logo1.svg';
-import fblogo from '../images/fblogo.svg';
-import googlelogo from '../images/googlelogo.svg';
+import HumanComponent from './HumanComponent';
+import logo from '../../images/logo1.svg';
+import circle from '../../images/fblogo.svg';
 
-class SignupContainer extends Component {
+class HumanContainer extends Component {
   render() {
   	let images = {
-  		fblogo: fblogo,
-  		googlelogo: googlelogo,
   		logo: logo,
+      circle: circle,
 
   	}
   	let links = [
-      {label: '',link:'#fblogin'},
-      {label: '',link:'#googlelogin'},
-      {label: 'Already a member? Log in',link:'#login'},
-      {label: 'Sign up',link:'#home'},
-
+      {label: 'Skip',link:'#Home'},
+      {label: 'Next',link:'#PetBasic'},
   	]
 
   	let titles = {
-  		title1: "What you may enjoy?",
-  		title2: "Good news! Your pets cannot wait for fun time. Let's create your account",
-  		title3: "Username",
-      title4: "Email address",
-  		title5: "Password",
-      title6: "Confirm password",
-  		title7: "I have read the Privacy and agree to the Terms of Service.",
-      title8: "Use Social Media Account to Sign Up",
-      title9: "Already a member? Log in",
-  	}
-  	let description = {
-  		des1: 'Create your special events for your cute and lovely pet(s)',
-  		des2: 'Join other fantastic events to meet more friends',
-  		des3: 'Share your happy moments with your pets',
-  		des4: 'Dive for more funny images and videos from other pets',
+  		title1: "Human profile",
+  		title2: "Pet basics",
+  		title3: "Pet detail",
+      title4: "Confirm",
+  		title5: "Hello! Please tell us a little bit about yourself.",
+      title6: "Age Range",
+      title7: "Gender",
+      title8: "Job Type",
+      title9: "City",
+      title10: "State",
+      title11: "Zip",
+      title12: "Self-Introduction (Optional)",
+
   	}
         return (
       <div>
-        <SignupComponent links={links} titles={titles} description={description} images={images}/>
+        <HumanComponent links={links} titles={titles} images={images}/>
       </div>
     );
   }
 }
 
-export default SignupContainer;
+export default HumanContainer;
