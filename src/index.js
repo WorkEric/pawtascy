1586dec9aca3285e5e72dd190c05653668a7feb5
Yixin Guo <blairguo1990@gmail.com>
2019-07-05 20:34:09 -0700

    router initiate

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Route, Link, BrowserRouter as Router } from 'react-router-dom';
 import './index.css';
 import HomeContainer from './Home/HomeContainer';
 import LoginContainer from './Login/LoginContainer';
@@ -10,9 +11,22 @@ import HumanContainer from './Signup/HumanProfile/HumanContainer.js';
 
 import * as serviceWorker from './serviceWorker';
 
+const routing = (
+    <Router>
+        <div>
+            <Route  exact path="/" component={ HomeContainer } />
+            <Route path="/login" component={ LoginContainer  } />
+            <Route path="/signup" component={ SignupContainer } />
+            <Route path="/confirmation" component={ ConfirmationContainer } />
+            <Route path="/pet-details" component={PetDetailsContainer}/>
+            <Route path="/human-profile" component={HumanContainer}/>
+        </div>
+    </Router>
+)
+
 //ReactDOM.render(<HomeContainer />, document.getElementById('root'));
 
-ReactDOM.render(<LoginContainer />, document.getElementById('root'));
+ReactDOM.render(routing, document.getElementById('root'));
 
 //ReactDOM.render(<SignupContainer />, document.getElementById('root'));
 
