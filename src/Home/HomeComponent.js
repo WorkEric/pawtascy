f1152b9f55e2f04c47014d18f1c0a65fef1f4561
Yixin Guo <blairguo1990@gmail.com>
2019-07-10 00:09:55 -0700

    redesign home page; make it responsive

--- src/Home/HomeComponent.js
+++ src/Home/HomeComponent.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import {Container, Row, Col, Image} from 'react-bootstrap';
+import {Container, Row, Col, Image, Button, CardDeck, Card} from 'react-bootstrap';
 
 import './Home.css';
 
@@ -11,48 +11,57 @@ class HomeComponent extends Component {
   }
 
   render() {
+      let cards = this.props.cards;
       return (
           <Container fluid className="home-container">
             <Row>
               <Image src={this.props.images.image1} className="home-background" />
             </Row>
             <Row>
-              <Col lg = {6}>
-                <h1 className="home_title1">{this.props.titles.title1}</h1>
-                <p className="home_des2">{this.props.description.des2}</p>
-                <div className="home_button2">
-                    <a href={this.props.sep_links[1].link}>{this.props.sep_links[1].label}</a>
-                </div>
+              <Col lg = {6} className="play">
+                <h1 className="play-together">{this.props.titles.playTogether}</h1>
+                <p className="play-content">{this.props.description.playContent}</p>
+                <Button variant="primary" className="events-button" > Host / find events </Button>
               </Col>
-              <Col lg = {6} >
-                <img src={this.props.images.image2} className="home_backimage2"/>
+              <Col lg = {6} className="animals">
+                <img src={this.props.images.animals} className="animal-image"/>
               </Col>
             </Row>
             <Row>
-              <Col lg = {6}>
-                <img src={this.props.images.image3} className="home_backimage3"/>
+              <Col lg = {6} className="sitter">
+                <img src={this.props.images.sitter} className="sitter-image"/>
               </Col>
-              <Col lg = {6}>
-                <h1 className="home_title2">{this.props.titles.title2}</h1>
-                <p className="home_des3">{this.props.description.des3}</p>
-                <div className="home_button3">
-                    <a href={this.props.sep_links[2].link}>{this.props.sep_links[2].label}</a>
-                </div>
+              <Col lg = {6} className="find-sitter">
+                <h1 className="find-sitter-title">{this.props.titles.findSitter}</h1>
+                <p className="find-sitter-content">{this.props.description.findSitterContent}</p>
+                <Button variant="primary" className="find-sitter-button" > Find sitter </Button>
               </Col>
             </Row>
             <Row>
-              <Col>
-                <h1 className="home_title3">{this.props.titles.title3}</h1>
-                <p className="home_des4">{this.props.description.des4}</p>
-                <div className="home_button4">
-                    <a href={this.props.sep_links[3].link}>{this.props.sep_links[3].label}</a>
-                </div>
+              <Col lg = {6} className="share-moments">
+                <h1 className="share-happiness">{this.props.titles.shareHappiness}</h1>
+                <p className="share-moments-content">{this.props.description.shareMomentsContent}</p>
+                <Button variant="primary" className="share-moments-button" > Post / Enjoy Happiness </Button>
               </Col>
-              <Col>
-                <img src={this.props.images.image4} className="home_backimage4"/>
+              <Col lg = {6} className="share-moments-image">
+                <img src={this.props.images.image4} className="dog-cat-image"/>
               </Col>
             </Row>
-
+            <Row className="about-us">
+              <h1 className="about-pawtascy"> About Pawtascy</h1>
+              <CardDeck>
+              {cards.map((card, index) => (
+                <Card key={index} className="card-item">
+                  <Image src={card.image} className="card-image"/>
+                  <Card.Body>
+                    <Card.Title className="card-title">{card.title}</Card.Title>
+                    <Card.Text className="card-content">
+                    {card.content}
+                    </Card.Text>
+                  </Card.Body>
+                </Card>))}
+              </CardDeck>
+            </Row>
           </Container>
       ); 
   }
--- src/Home/HomeContainer.js
+++ src/Home/HomeContainer.js
@@ -1,23 +1,19 @@
 import React, { Component } from 'react';
+
 import HomeComponent from './HomeComponent';
 import logo from '../images/logo1.svg';
 import image1 from '../images/bg.png';
-import image2 from '../images/homepage2.jpg';
-import image3 from '../images/homepage3.jpg';
+import animals from '../images/animals.png';
+import sitter from '../images/homepage3.jpg';
 import image4 from '../images/homepage4.jpg';
+import howImage from '../images/how-image.png'
+import whoImage from '../images/who-image.png'
+import whatImage from '../images/what-image.png'
 import Footer from '../Footer/Footer.js';
 import Header from '../Header/Header.js';
 
 class HomeContainer extends Component {
   render() {
-    let links = [
-      {label: 'Home',link:'#home',active:true},
-      {label: 'Share Moments',link:'#share'},
-      {label: 'Events',link:'#events'},
-      {label: 'Create Events',link:'#create'},
-      {label: 'Sign up',link:'#signup'},
-      {label: 'Login',link:'#login'},
-    ]
     let sep_links = [
       {label: 'sign up your pet',link:'#signpet'},
       {label: 'Host/find events',link:'#events'},
@@ -25,34 +21,38 @@ class HomeContainer extends Component {
       {label: 'Post/Enjoy Happiness',link:'#share'},
     ]
     let titles = {
-      title1: 'Play Together',
-      title2: 'Find Sitter',
-      title3: 'Share Happiness',
+      playTogether: 'Play Together',
+      findSitter: 'Find Sitter',
+      shareHappiness: 'Share Happiness',
       title4: 'About Pawtascy',
       title5: 'Who we are',
       title6: 'How we do',
       title7: 'What is our mission'
     }
     let description = {
-      des1: 'Let your pet meet with others while you make friends as well',
-      des2: 'Host or attend a nearby pet meeting event. You will choose the ones you and your pets like. Or, you can host a special event to meet more pets owners who have the same interest in common with you and make your pets happy by playing with others.',
-      des3: 'We know how stressful it is to leave your pets at home, alone. Find experience sitters to take care of your pets while you are away from home to keep your furry little loved ones happy, safe and fed',
-      des4: 'Happy moment is not just your own joy. Tons of people want to see the lovely and funny time of your pets. You can share your pet usual life, hosted events, happy moment records while attending the events and anything you like.',
+      playContent: 'Host or attend a nearby pet meeting event. You will choose the ones you and your pets like. Or, you can host a special event to meet more pets owners who have the same interest in common with you and make your pets happy by playing with others.',
+      findSitterContent: 'We know how stressful it is to leave your pets at home, alone. Find experience sitters to take care of your pets while you are away from home to keep your furry little loved ones happy, safe and fed',
+      shareMomentsContent: 'Happy moment is not just your own joy. Tons of people want to see the lovely and funny time of your pets. You can share your pet usual life, hosted events, happy moment records while attending the events and anything you like.',
       des5: 'We care about the',
 
     }
     let images = {
       image1: image1,
-      image2: image2,
-      image3: image3,
+      animals: animals,
+      sitter : sitter,
       image4: image4,
       logo: logo,
 
     }
+    let cards = [
+      {title: "who we are", content: "We care about the social life of your pets.  Pawtascy gives the chance to pet owners & lovers in which they bring their pets to play with each other and host some unique parties. At the same time, users can make more friends who have pet interest in common.", image:whoImage},
+      {title: "How we do", content: "We provide this website for pet owners and lovers to create or join the nearby pet communities and events. In addition, pet owners can also find sitters while they are away from home. They can also share their pet life with others. ", image:howImage},
+      {title: "What's our mission", content: "We are aimed to providing a eco-friendly, happy and safe social community for the people who have or love pets. We consider pets as friends and want to bring convenience for pet owners to pat, breed and take care of their pets.", image:whatImage}
+    ]
     return (
       <div>
         <Header />
-        <HomeComponent links={links} sep_links={sep_links} titles={titles} description={description} images={images}/>
+        <HomeComponent sep_links={sep_links} titles={titles} description={description} images={images} cards={cards}/>
         <Footer />
       </div>
     );
