d845f6216824bc101ac1359e0624a157e80f80ec
ewangdx <ruiwmichael@gmail.com>
2019-07-12 16:31:53 -0700

    handle exception

--- server/app.js
+++ server/app.js
@@ -10,7 +10,6 @@ const schema = require('./schema.js');
 const models = require('./models/index.js')
 
 const PORT = process.env.NODE_ENV === 'production' ? 5000 : 9000;
-const path = require('path');
 
 
 // Create an express server
